3ec141de1a26e002792ab0f46b40287ac66dc9e9
魏齐鑫 <weiqixin@wdai.com>
2018-12-28 15:45:18 +0800

    初始化准备

---
+++ src/main/java/com/wqx/test/filetype/FileType.java
@@ -0,0 +1,10 @@
+package com.wqx.test.filetype;
+
+import lombok.Data;
+
+@Data
+public class FileType {
+
+    private String type;
+
+}
---
+++ src/test/java/com/wqx/test/TestCase.java
@@ -0,0 +1,15 @@
+package com.wqx.test;
+
+import org.junit.Test;
+
+public class TestCase {
+    @Test
+    public void test() {
+
+    }
+
+    @Test
+    public void test1() {
+
+    }
+}
